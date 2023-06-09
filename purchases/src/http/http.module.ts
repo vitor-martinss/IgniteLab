68a6afd8bbc3aac932517ab606d2dda40ce27323
vitor-martinss <vitormartinsweb@gmail.com>
2023-06-09 14:14:24 +0100

    feat: config graphQL

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TestController } from './test.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [TestController],
 })
 export class HttpModule {}
