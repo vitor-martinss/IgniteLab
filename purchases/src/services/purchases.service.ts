a455a5973cbf89247ebd60df127c9403a47a200a
vitor-martinss <vitormartinsweb@gmail.com>
2023-06-18 12:52:54 +0100

    feat: kafka config - purchases

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 import slugify from 'slugify';
+import { KafkaService } from '../messaging/kafka.service';
 
 interface CreatePurchaseParams {
   customerId: string;
@@ -9,9 +10,7 @@ interface CreatePurchaseParams {
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {
-    this.prisma = prisma;
-  }
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   listAllPurchases() {
     return this.prisma.product.findMany({
@@ -43,11 +42,30 @@ export class PurchasesService {
       throw new Error('Product not found');
     }
 
-    return await this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: {
         customerId,
         productId,
       },
     });
+
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
