8b06e75afd6c1b8190c5515a3f57f2c4304c9f49
vitor-martinss <vitormartinsweb@gmail.com>
2023-06-21 11:16:57 +0100

    feat: classroom services

--- classroom/src/messaging/controllers/purchases.controller.ts
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -1,5 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { CoursesService } from '../../services/courses.service';
+import { EnrollmentsService } from '../../services/enrollments.service';
+import { StudentsService } from '../../services/students.service';
 
 interface Customer {
   authUserId: string;
@@ -18,8 +21,36 @@ interface PurchaseCreatedPayload {
 
 @Controller()
 export class PurchasesController {
+  constructor(
+    private studentsService: StudentsService,
+    private coursesService: CoursesService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
   @EventPattern('purchases.new-purchase')
   async purchasesCreated(@Payload('value') payload: PurchaseCreatedPayload) {
-    console.log(payload);
+    let student = await this.studentsService.getStudentByAuthUserId(
+      payload.customer.authUserId,
+    );
+
+    if (!student) {
+      student = await this.studentsService.create({
+        authUserId: payload.customer.authUserId,
+      });
+    }
+
+    let course = await this.coursesService.getCourseBySlug(
+      payload.product.slug,
+    );
+
+    if (!course) {
+      course = await this.coursesService.createCourse({
+        title: payload.product.title,
+      });
+    }
+
+    await this.enrollmentsService.createEnrollment({
+      courseId: course.id,
+      studentId: student.id,
+    });
   }
 }
--- classroom/src/messaging/messaging.module.ts
+++ classroom/src/messaging/messaging.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PurchasesController } from './controllers/purchases.controller';
+import { StudentsService } from '../services/students.service';
+import { CoursesService } from '../services/courses.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchasesController],
+  providers: [StudentsService, CoursesService, EnrollmentsService],
 })
 export class MessagingModule {}
